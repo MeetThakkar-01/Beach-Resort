7faeef4b767ba02602452f2f5404bdb716d3d9b4
Meet Thakkar <meetthakkar05@gmail.com>
2020-11-25 14:11:35 +0530

    removed unused imports

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
 import Title from "./Title";
-import {
-  FaCocktail,
-  FaHiking,
-  FaShuttleVan,
-  FaBeer,
-  FaAccessibleIcon,
-} from "react-icons/fa";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 class Services extends Component {
   state = {
     services: [
